1f1dec29c2d7a504424348ce4ee4ec4f23907687
Jimmy Kuang <jimmy.kuang96@gmail.com>
2024-10-25 19:18:23 -0700

    Add basic game to work on HTML

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ const generatePlayers = () => {
   }
 
   const swapPlayers = () => playerFlag = !playerFlag;
+  const getPlayers = () => [player1, player2];
 
-  return ({ getCurrentPlayer, swapPlayers })
+  return ({ getCurrentPlayer, getPlayers, swapPlayers })
 }
 
 // Generate the board for the game
@@ -97,34 +98,100 @@ const board = () => {
   return ({ getBoard, placeMark, checkForWinner, getNumTurns, increaseNumTurns })
 }
 
+function generateHTMLGrid(players, gameBoard) {
+  const container = document.querySelector(".container");
+  const player1 = players.getPlayers()[0];
+  const player2 = players.getPlayers()[1];
+
+  // Clear the content div
+  container.removeChild(container.lastChild);
+
+  // Create content 
+  const content = document.createElement("div");
+  content.classList.add("content");
+  container.appendChild(content);
+
+  // Build player 1 scoreboard
+  const player1Info = document.createElement("div");
+  player1Info.classList.add("player-info");
+  player1Info.innerText = player1.getPlayerName();
+  content.appendChild(player1Info);
+
+  const player1Score = document.createElement("div");
+  player1Score.classList.add("player-info");
+  player1Score.innerText = player1.getPlayerScore();
+  player1Info.appendChild(player1Score);
+
+  // Build board back
+  const grid = document.createElement("div");
+  grid.classList.add("grid-container");
+  content.appendChild(grid);
+
+  //Generate the cells of the grid 
+  for (let i = 0; i < 3; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.add("grid-row");
+
+    for (let j = 0; j < 3; j++) {
+      const newCell = document.createElement("div");
+      newCell.classList.add("grid-cell");
+      newCell.addEventListener("click", () => {
+        let currentPlayer = players.getCurrentPlayer();
+        
+        const placementResult = gameBoard.placeMark(currentPlayer.getPlayerSymbol(), i, j);
+        
+        if (placementResult)  {
+          newCell.innerText = players.getCurrentPlayer().getPlayerSymbol();
+
+          if (gameBoard.checkForWinner()) {
+            currentPlayer.updatePlayerScore();
+            generateGameOver();
+          } else if (gameBoard.getNumTurns() === 8) {
+            generateGameOver();
+          }
+          
+          players.swapPlayers();
+          gameBoard.increaseNumTurns();
+        }
+      });
+      newRow.appendChild(newCell);
+    }
+
+    grid.appendChild(newRow);
+  }
+
+  // Build player 2 scoreboard
+  const player2Info = document.createElement("div");
+  player2Info.classList.add("player-info");
+  player2Info.innerText = player2.getPlayerName();
+  content.appendChild(player2Info);
+
+  const player2Score = document.createElement("div");
+  player2Score.classList.add("player-info");
+  player2Score.innerText = player2.getPlayerScore();
+  player2Info.appendChild(player2Score);
+
+
+  function generateGameOver() {
+    const gameOver = document.createElement("div");
+    gameOver.classList.add("game-over");
+    grid.appendChild(gameOver);
+
+    const replayButton = document.createElement("button");
+    replayButton.innerText = "PLAY AGAIN"
+    replayButton.classList.add("replay-button");
+    replayButton.addEventListener("click", () => {
+      generateHTMLGrid(players, board());
+    })
+    gameOver.appendChild(replayButton);
+  }
+}
+
 // IIFE new game function
 // generates a board and players
+// HTML board generation placed in here to get access to the board and player functions
 (function newGame(){
   const gameBoard = board();
   const players = generatePlayers();
-  let gameOver = false;
-
-  while (!gameOver) {
-    let currentPlayer = players.getCurrentPlayer();
-
-    const row = prompt(`It is player ${currentPlayer.getPlayerName()}'s turn! What row would you like to place your mark?`);
-    const col = prompt(`It is player ${currentPlayer.getPlayerName()}'s turn! What column would you like to place your mark?`);
-
-    const placementResult = gameBoard.placeMark(currentPlayer.getPlayerSymbol(), row, col);
-
-    if (placementResult)  {
-      if (gameBoard.checkForWinner()) {
-        gameOver = true;
-        alert("Winner!");
-      } else if (gameBoard.getNumTurns() === 8) {
-        gameOver = true;
-        alert("Draw!");
-      }
-  
-      players.swapPlayers();
-      gameBoard.increaseNumTurns();
-    } else {
-      alert("Invalid cell");
-    }
-  }
+  generateHTMLGrid(players, gameBoard);
 })();
